01af22cd550651081bfc4e75688b5642eeb9f40c
MahnoorFatima02 <mahnoorfatima@Mahnoors-MacBook-Air.local>
2025-05-23 16:16:55 +0300

    not found  updated

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../app.css';  // Import the CSS file
 
 const NotFound = () => {
   return (
--- frontend/src/components/ShellForm.jsx
+++ frontend/src/components/ShellForm.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import axiosInstance from '../redux/axiosInstance';
