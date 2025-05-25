9586740023711be58aafb17a5bbcc1a8ec4e696e
MahnoorFatima02 <mahnoorfatima@Mahnoors-MacBook-Air.local>
2025-05-25 11:07:11 +0300

    backend url updated

--- frontend/src/redux/axiosInstance.jsx
+++ frontend/src/redux/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "./store";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://seashellcollection-backend.onrender.com/api/v1",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
